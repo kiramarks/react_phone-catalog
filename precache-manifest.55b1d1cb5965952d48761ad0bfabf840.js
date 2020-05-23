a1fd02b87543b829a06e87fc7ff539a73a858303
Kira Marks <serafita22@gmail.com>
2020-05-23 19:26:26 +0300

    Updates

---
+++ precache-manifest.55b1d1cb5965952d48761ad0bfabf840.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b93eca3f24dac83e58c6b4ec313236",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a7a88b381a74ab13640d",
+    "url": "./static/css/main.c215346f.chunk.css"
+  },
+  {
+    "revision": "5140fcd7d13e5c9ab802",
+    "url": "./static/js/2.e87d445f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.e87d445f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7a88b381a74ab13640d",
+    "url": "./static/js/main.29d74692.chunk.js"
+  },
+  {
+    "revision": "13d57103233adbc95222",
+    "url": "./static/js/runtime-main.7c3e9311.js"
+  },
+  {
+    "revision": "482e37260cd688576287f1976e958e0b",
+    "url": "./static/media/Mont-Bold.482e3726.otf"
+  },
+  {
+    "revision": "f0f845588eef439ac6ecc9b6ab42191c",
+    "url": "./static/media/Mont-Regular.f0f84558.otf"
+  },
+  {
+    "revision": "d41a520b1802421f24e21279736e1871",
+    "url": "./static/media/Mont-SemiBold.d41a520b.otf"
+  },
+  {
+    "revision": "a860aa3c8fe78a96b03706400cc4790d",
+    "url": "./static/media/cart-default.a860aa3c.svg"
+  },
+  {
+    "revision": "a8ef0cee1e5ba1bdbf894a31b68a9685",
+    "url": "./static/media/favorite_checked.a8ef0cee.svg"
+  },
+  {
+    "revision": "9df0e3a37e3e8c5b0b332f85fb6b7843",
+    "url": "./static/media/favorite_default.9df0e3a3.svg"
+  },
+  {
+    "revision": "e81aaeac5a719d0c5c0bee203ffbdbbc",
+    "url": "./static/media/footer-top-arrow.e81aaeac.svg"
+  },
+  {
+    "revision": "a1a856a2ff176b420034851f432fb25a",
+    "url": "./static/media/left.a1a856a2.svg"
+  },
+  {
+    "revision": "51367df4d5a86477aff6631f3aee1433",
+    "url": "./static/media/right.51367df4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f96163bf66b4bc18893bcedc683b7c29.js"
+  "./precache-manifest.55b1d1cb5965952d48761ad0bfabf840.js"
 );
 
 self.addEventListener('message', (event) => {
